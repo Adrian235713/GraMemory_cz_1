deac8a3c1e84a6dc7b95a2f86170753d8149df83
Adrian Poniatowski <66631454+Adrian235713@users.noreply.github.com>
2022-04-04 10:20:57 +0200

    Cz 1 naszej gry
    cz 1 naszej gry

---
+++ Gra Memory/Form1.cs
@@ -0,0 +1,41 @@
+﻿using MemoryGame;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gra_Memory
+{
+    public partial class MemoryForm : Form
+    {
+        private GameSettings _settings;
+
+        MemoryCard _pierwsza = null;
+        MemoryCard _drugi = null;
+
+        public MemoryForm()
+        {
+            InitializeComponent();
+            _settings = new GameSettings();
+            UstawKontrolki();
+        }
+
+        private void UstawKontrolki()
+        {
+            panelKart.Width = _settings.Bok * _settings.Kolumny;
+            panelKart.Height = _settings.Bok * _settings.Wiersze;
+            Width = panelKart.Width + 40;
+            Height = panelKart.Height + 100;
+            lblStartInfo.Text = $"Początek gry za {_settings.CzasPodgladu}.";
+            lblPunktyWartosc.Text = _settings.AktualnePunkty.ToString();
+            lblCzasWartosc.Text = _settings.CzasGry.ToString();
+            lblStartInfo.Visible = true;
+        }
+
+    }
+}
---
+++ Gra Memory/GameSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_Memory
+{
+    class GameSettings
+    {
+        public int CzasGry;
+        public int CzasPodgladu;
+        public int MaxPunkty;
+        public int Wiersze;
+        public int Kolumny;
+        public int Bok;
+        public int AktualnePunkty;
+
+        public string PlikLogo = $@"{AppDomain.CurrentDomain.BaseDirectory}\img\logo.jpg";
+        public string FolderObrazki = $@"{AppDomain.CurrentDomain.BaseDirectory}\img\memory";
+
+        public GameSettings()
+        {
+            UstawStartowe();
+        }
+
+        public void UstawStartowe()
+        {
+            CzasPodgladu = 5;
+            CzasGry = 60;
+            MaxPunkty = 0;
+            Wiersze = 4;
+            Kolumny = 6;
+            Bok = 150;
+            AktualnePunkty = 0;
+        }
+
+    }
+}
